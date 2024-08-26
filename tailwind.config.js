df02a252a45d65a21b87e1270d52b303b7fe308d
Bhattii <imran5244513@gmail.com>
2024-08-26 05:59:11 -0700

    card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        upDown: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        upDown: 'upDown 1s ease-in-out infinite',
+      },
+    },
   },
+  variants: {},
   plugins: [],
-}
+};
