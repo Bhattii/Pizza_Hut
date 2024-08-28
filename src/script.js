bf766070cddb7c5b1729cc4669ec8ce447cb1255
Bhattii <imran5244513@gmail.com>
2024-08-28 10:20:13 -0700

    footer

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 
-
+// Slider
 let myIndex = 0;
 carousel();
 
@@ -13,3 +13,40 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 5000);    
 }
+
+// active link
+
+  document.addEventListener("DOMContentLoaded", function () {
+            const sections = document.querySelectorAll('section');
+            const navLinks = document.querySelectorAll('nav a');
+
+            function changeActiveLink() {
+                let index = sections.length;
+
+                while (--index && window.scrollY + 150 < sections[index].offsetTop) {}
+
+                navLinks.forEach((link) => link.classList.remove('bg-orange-600', 'text-white'));
+                navLinks[index].classList.add('bg-orange-600', 'text-white');
+            }
+
+            function handleLinkClick(event) {
+                event.preventDefault();
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+                const offsetPosition = targetElement.offsetTop - 140;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+
+                navLinks.forEach((link) => link.classList.remove('bg-orange-600', 'text-white'));
+                this.classList.add('bg-orange-600', 'text-white');
+            }
+
+            navLinks.forEach(link => {
+                link.addEventListener('click', handleLinkClick);
+            });
+
+            window.addEventListener('scroll', changeActiveLink);
+        });
